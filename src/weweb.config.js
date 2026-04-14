f76d210d424779a1d3fe95efd31774b08b18f771
datageekautomacao-lgtm <datageekautomacao@gmail.com>
2026-04-14 15:39:06 -0300

    Add weweb.config.js for component configuration

---
+++ src/weweb.config.js
@@ -0,0 +1,11 @@
+export default {
+  components: {
+    LoginAgendaPro: {
+      path: './src/LoginAgendaPro.vue',
+      name: 'Login AgendaPro',
+      description: 'Componente de login para agenda profissional',
+      icon: 'mdiLock',
+      category: 'Custom'
+    }
+  }
+}
